293dfaf0dc467b24e45290d104ef80419dcf1c8e
Jamie Brynes <jamie.brynes@samsara.com>
2025-06-03 17:04:08 +0100

    flake.nix: pin to nix-darwin/24.11

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-24.11-darwin";
     darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:LnL7/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
